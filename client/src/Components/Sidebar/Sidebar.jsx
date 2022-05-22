1f902bf6738ef6a48660110fbf487261cfa315c4
Noman <noman.khan3701@gmail.com>
2022-05-22 18:16:55 +0530

    Made the sidebar

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { AiOutlineAlignLeft } from "react-icons/ai";
 import { BiX } from "react-icons/bi";
+import { AiOutlineFolderOpen, AiOutlineAlignLeft } from "react-icons/ai";
+import { MdOutlineHome } from "react-icons/md";
+import stb from "../../assets/stb.svg";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   return (
     <div className="sidebar">
@@ -13,10 +15,27 @@ const Sidebar = () => {
         onClick={() => setToggle(true)}
       />
       <div className={`sidebar-toggle ${toggle ? "active" : ""}`}>
-        <BiX className="toggle-icon" onClick={() => setToggle(false)} />
-
-        <div>
-            {/* All the code for sidebar here */}
+        <div className="sidebar-top">
+          <BiX className="toggle-i" onClick={() => setToggle(false)} />
+          <img src={stb} alt="" />
+        </div>
+        <div className="sidebar-links">
+          <div className="sidebar-link active">
+            <MdOutlineHome />
+            <div className="link-content">Home</div>
+          </div>
+          <div className="sidebar-link">
+            <AiOutlineFolderOpen />
+            <div className="link-content">Files</div>
+          </div>
+          <div className="sidebar-link">
+            <MdOutlineHome />
+            <div className="link-content">Home</div>
+          </div>
+          <div className="sidebar-link">
+            <AiOutlineFolderOpen />
+            <div className="link-content">Files</div>
+          </div>
         </div>
       </div>
     </div>
