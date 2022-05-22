7e3d8a9173a3b0f667680add2439998a60d2a86d
Noman <noman.khan3701@gmail.com>
2022-05-22 17:33:31 +0530

    Made the card horizontal

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
+import spidy from "../../assets/spidy.png";
 
 const Card = () => {
   return (
-    <div className='card-container'>
-      <div className="card">
-        <div className="box">
-          <div className="content">
-            <h2>01</h2>
-            <h3>Card One</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus autem recusandae, quis, nemo debitis iste, reprehenderit illo voluptatibus nostrum est magni animi sunt. </p>
-            <a href="#">Read More</a>
-          </div>
+    <div className="card">
+      <div className="box">
+        <div className="content">
+          <img src={spidy} alt="" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            autem recusandae, quis, nemo debitis iste, reprehenderit illo
+            voluptatibus nostrum est magni animi sunt.{" "}
+          </p>
+          <a href="#">Download</a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
